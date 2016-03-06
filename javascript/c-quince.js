8be4abc2e50bd8caf9a56fb8e0bd8e6a8e508203
Stephen Riesenberg <sjohnr@hotmail.com>
2016-03-06 17:18:23 -0600

    Much better attempt! It's working!

--- javascript/c-quince.js
+++ javascript/c-quince.js
@@ -1,21 +1,40 @@
 cquince = {};
 
 (function() {
+    "use strict";
+
     cquince.Sprite = function(canvas) {
         var spriteSheet = new createjs.SpriteSheet({
-            images: ["images/sprite_sheet.png"],
+            images: ["images/sprite_sheet_3.png", "images/down_profile_right.png"],
             frames: {
-                width: 100, height: 175, count: 4, spacing: 50
+                width: 175, height: 175
             },
             animations: {
-                front: 0, right: 1, back: 2, left: 3, spin: [0, 1, 2, 3]
+                front: 0, right: 1, back: 2, left: 3,
+                spin: [0, 3],
+                peck: {
+                    frames: [
+                        4, 4, 4, 4, 5, 6,
+                        7, 8, 9, 9, 8, 7,
+                        7, 8, 9, 9, 8, 7,
+                        7, 8, 9, 9, 8, 7,
+                        6, 5, 4, 4, 4, 4
+                    ],
+                    speed: 4,
+                    next: "right"
+                },
+                bow: {
+                    frames: [4, 5, 6, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 6, 5, 4],
+                    speed: 3,
+                    next: "right"
+                }
             },
-            framerate: 2
+            framerate: 8
         });
-
         var sprite = new createjs.Sprite(spriteSheet, "front");
         sprite.x = 100;
         sprite.y = 100;
+        sprite.on("play", playNext, this);
 
         var stage = new createjs.Stage(canvas);
         stage.addChild(sprite);
@@ -27,70 +46,108 @@ cquince = {};
         this.sprite = sprite;
         this.stage = stage;
         this.tween = createjs.Tween(sprite, { paused: true });
+        this.queue = [];
     };
 
     cquince.Sprite.prototype = {
-        move: function(x, y, animation) {
-            var newX = this.sprite.x + x;
-            var newY = this.sprite.y + y;
-            if (checkBound(newX) && checkBound(newY)) {
-                var tween = createjs.Tween.get(this.sprite, { paused: true })
-                    .call(this.animate, [animation], this)
-                    .wait(50)
-                    .to({ x: newX, y: newY }, 500);
-                if (createjs.Tween.hasActiveTweens()) {
-                    console.log("active");
-                    this.tween.play(tween);
-                } else {
-                    this.tween = tween;
-                    tween.setPaused(false);
-                }
-            }
-        },
         animate: function(animation) {
-            this.sprite.gotoAndPlay(animation);
+            queue(function() {
+                console.log("animate " + animation);
+                this.sprite.gotoAndPlay(animation);
+                playNext.call(this);
+            }, this, []);
+            return this;
         },
-        moveRight: function() {
-            this.move(100, 0, "right");
-        },
-        moveDown: function() {
-            this.move(0, 100, "front");
-        },
-        moveLeft: function() {
-            this.move(-100, 0, "left");
-        },
-        moveUp: function() {
-            this.move(0, -100, "back");
+        move: function(x, y) {
+            queue(function() {
+                console.log("move " + x + "," + y);
+                createjs.Tween.get(this.sprite)
+                    .wait(50)
+                    .to({
+                        x: this.sprite.x + x,
+                        y: this.sprite.y + y
+                    }, 500)
+                    .call(playNext, [], this);
+            }, this);
+            return this;
         },
         spin: function() {
-            this.sprite.gotoAndPlay("spin");
+            this.animate("spin");
+            return this;
+        },
+        peck: function() {
+            this.animate("peck");
+            return this;
+        },
+        bow: function() {
+            this.animate("bow");
+            return this;
         },
         stop: function() {
-            this.sprite.gotoAndStop("front");
+            this.animate("front");
+            return this;
         },
-        execute: function(textarea) {
-            var code = document.getElementById(textarea).value;
-            eval(code);
-            //var code = document.getElementById(textarea).value.split(";");
-            //for (var i = 0; i < code.length - 1; i++) {
-            //    var f = code[i].trim();
-            //    f = f.substring(0, f.length - 2);
-            //    this[f]();
-            //}
-
-            //var head = document.getElementsByTagName("head")[0];
-            //var script = document.createElement("script");
-            //script.type = "text/javascript";
-            //script.innerHTML = document.getElementById(textarea).value;
-            //head.appendChild(script);
+        moveUp: function() {
+            this.animate("back").move(0, -100);
+            return this;
+        },
+        moveRight: function() {
+            this.animate("right").move(100, 0);
+            return this;
+        },
+        moveDown: function() {
+            this.animate("front").move(0, 100);
+            return this;
+        },
+        moveLeft: function() {
+            this.animate("left").move(-100, 0);
+            return this;
+        },
+        play: function() {
+            this.sprite.dispatchEvent("play");
+            return this;
+        },
+        execute: function(workspace) {
+            eval($(workspace).value);
+            this.play();
+            return this;
         }
     };
 
-    function checkBound(coord) {
-        return (coord <= 700 && coord >= 0);
+    function $(id) {
+        return document.getElementById(id);
+    }
+
+    function queue(f, context, args) {
+        context.queue.push(function() {
+            f.call(context, args);
+        });
+    }
+
+    function playNext() {
+        if (this.queue.length > 0) {
+            var f = this.queue.shift();
+            f.call(this, []);
+        }
     }
 })();
 
 function init() {
     window.sprite = new cquince.Sprite("demoCanvas");
+}
+
+function moveUp() {
+    sprite.moveUp();
+}
+
+function moveRight() {
+    sprite.moveRight();
+}
+
+function moveDown() {
+    sprite.moveDown();
+}
+
+function moveLeft() {
+    sprite.moveLeft();
 }
